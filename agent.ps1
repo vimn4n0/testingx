60c88457d9d3bbf2ec8ca3d890fc9b3c89e894d5
vimn4n0 <vim.n4n0@gmail.com>
2024-07-02 19:55:33 +0700

    testing

--- agent.ps1
+++ agent.ps1
@@ -7,18 +7,6 @@ Remove-Item -Path "Sysmon.zip" -Recurse
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml" -OutFile "C:\Program Files\Sysmon\config.xml"
 Invoke-Expression "C:\Program Files\Sysmon\Sysmon64.exe -accepteula -i 'C:\Program Files\Sysmon\config.xml'"
 
-Function Sleep-Progress($TotalSeconds) {
-    $Counter = 0;
-    for ($i = 0; $i -lt $TotalSeconds; $i++) {
-        $Progress = [math]::Round(100 - (($TotalSeconds - $Counter) / $TotalSeconds * 100));
-        Write-Progress -Activity "Waiting..." -Status "$Progress% Complete:" -SecondsRemaining ($TotalSeconds - $Counter) -PercentComplete $Progress;
-        Start-Sleep 1
-        $Counter++;
-    }   
-}
- 
- Sleep-Progress 5
- 
 # Download and install NineFort Agent
 New-Item -ItemType Directory -Force -Path "C:\Program Files\NineFort\NineFort Agent"
 Invoke-WebRequest -Uri "https://cdn.ninefort.com/agent_updates/release/installer/v1.0.3/ninefort_agent_installer.exe" -OutFile "C:\Program Files\NineFort\NineFort Agent\ninefort_agent_installer.exe"
