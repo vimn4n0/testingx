77c282b69660e1ace1dfb8b34b837bb9dd07a2a7
vimn4n0 <vim.n4n0@gmail.com>
2024-07-02 19:48:45 +0700

    testing

--- agent.ps1
+++ agent.ps1
@@ -1,5 +1,11 @@
-# Disable ExecutionPolicy
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;
+# Download and install Sysmon
+Invoke-WebRequest -Uri "https://download.sysinternals.com/files/Sysmon.zip" -OutFile "Sysmon.zip"
+Expand-Archive -Path "Sysmon.zip" -DestinationPath "C:\Program Files\Sysmon"
+Remove-Item -Path "Sysmon.zip" -Recurse
+
+# Configure Sysmon
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml" -OutFile "C:\Program Files\Sysmon\config.xml"
+Invoke-Expression "C:\Program Files\Sysmon\Sysmon64.exe -accepteula -i 'C:\Program Files\Sysmon\config.xml'"
 
 Function Sleep-Progress($TotalSeconds) {
     $Counter = 0;
@@ -12,20 +18,7 @@ Function Sleep-Progress($TotalSeconds) {
 }
  
  Sleep-Progress 5
-
-
-
-# Download and install Sysmon
-Invoke-WebRequest -Uri "https://download.sysinternals.com/files/Sysmon.zip" -OutFile "Sysmon.zip"
-Expand-Archive -Path "Sysmon.zip" -DestinationPath "C:\Program Files\Sysmon"
-Remove-Item -Path "Sysmon.zip" -Recurse
-
-# Configure Sysmon
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml" -OutFile "C:\Program Files\Sysmon\config.xml"
-Invoke-Expression "C:\Program Files\Sysmon\Sysmon64.exe -accepteula -i 'C:\Program Files\Sysmon\config.xml'"
-
-
-
+ 
 # Download and install NineFort Agent
 New-Item -ItemType Directory -Force -Path "C:\Program Files\NineFort\NineFort Agent"
 Invoke-WebRequest -Uri "https://cdn.ninefort.com/agent_updates/release/installer/v1.0.3/ninefort_agent_installer.exe" -OutFile "C:\Program Files\NineFort\NineFort Agent\ninefort_agent_installer.exe"
